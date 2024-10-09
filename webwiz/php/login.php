4824a566ee1c92247a78d454e4f587d630770218
Jeong Seong Bin <tjdqls3124@gmail.com>
2024-10-09 17:01:58 +0900

    Update login.php

--- webwiz/php/login.php
+++ webwiz/php/login.php
@@ -56,7 +56,7 @@ $conn->close();
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>로그인 확인 - FeelFlow</title>
-    <link rel="stylesheet" href="/webwiz/webwiz/css/mainstyle.css">
+    <link rel="stylesheet" href="../css/mainstyle.css">
 </head>
 
 <body>
