69184fa29118a6911ea21d50be8cf9e76fe96a5a
Jeong Seong Bin <tjdqls3124@gmail.com>
2024-10-09 17:01:44 +0900

    Update join_member.php

--- webwiz/php/join_member.php
+++ webwiz/php/join_member.php
@@ -54,7 +54,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+KR:wght@400;700&display=swap" rel="stylesheet">
-    <link rel="stylesheet" href="/webwiz/webwiz/css/joinmemstyle.css" />
+    <link rel="stylesheet" href="../css/joinmemstyle.css" />
 </head>
 
 <body>
