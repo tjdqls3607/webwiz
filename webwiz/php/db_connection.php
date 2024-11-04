6c5fdea58cab03c62f9f1753ef4590175691db53
Jeong Seong Bin <tjdqls3124@gmail.com>
2024-11-04 14:39:42 +0900

    db 연결 php파일 (필요시 삭제 가능)

---
+++ webwiz/php/db_connection.php
@@ -0,0 +1,14 @@
+<?php
+$servername = "localhost"; // MySQL 서버 주소
+$username = "root"; // MySQL 사용자 이름
+$password = ""; // MySQL 비밀번호
+$dbname = "feelcontent"; // 데이터베이스 이름
+
+// MySQLi 객체를 이용해 데이터베이스 연결
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// 연결 체크
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+?>
