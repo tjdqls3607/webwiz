8bbdc369857e97aa3c086b8c6a57a5b515a423a4
Jeong Seong Bin <tjdqls3124@gmail.com>
2024-11-06 17:00:33 +0900

    css 변경 및 comment textarea -> text 타입 변경

--- webwiz/php/edit_info.php
+++ webwiz/php/edit_info.php
@@ -1,13 +1,25 @@
 <?php
 session_start();
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
+
 if (!isset($_SESSION['user_nickname'])) {
     echo "<script>alert('로그인이 필요합니다. 로그인 페이지로 이동합니다.');</script>";
     header("Location: login.php");
     exit();
 }
+
+echo '<div class="menu-bar2 menu-bar-item2">';
+echo '<a href="../php/feelflow_home.php" class="menu-bar1 menu-button menu-bar-item1">FeelFlow</a>';
+
+echo '<div class="menu-bar2">';
+echo '<a href="../php/logout.php" class="menu-button menu-bar-item2">로그아웃</a>';
+echo '<a href="../html/diary_write.html" class="menu-button menu-bar-item2">새로운 일기</a>';
+echo '<a href="../html/emotion_calender.html" class="menu-button menu-bar-item2">감정 캘린더</a>';
+echo '<a href="#emotion_dash_board" class="menu-button menu-bar-item2">감정분석 대시보드</a>';
+echo '<a href="#content" class="menu-button menu-bar-item2">콘텐츠 추천</a>';
+echo '<a href="#matching" class="menu-button menu-bar-item2">사용자 매칭</a>';
+echo '</div>';
+echo '</div>';
+
 
 // 데이터베이스 연결 설정 (필요한 정보로 수정하세요)
 $servername = "localhost";
@@ -57,6 +69,8 @@ $conn->close();
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Edit Information</title>
+    <link rel="stylesheet" href="../css/loginstyle.css">
+    <link rel="stylesheet" href="../css/menu_bar.css">
 </head>
 <body>
 <h2>회원 정보 수정</h2>
@@ -64,7 +78,7 @@ $conn->close();
     <label for="nickname">닉네임:</label>
     <input type="text" id="nickname" name="nickname" value="<?php echo $_SESSION['user_nickname']; ?>">
     <label for="comment">한마디:</label>
-    <textarea id="comment" name="comment"><?php echo $_SESSION['user_comment']; ?></textarea>
+    <input type="text" id="comment" name="comment" value="<?php echo $_SESSION['user_comment']; ?>">
     <button type="submit"> 저장하기</button>
 </form>
 </body>
